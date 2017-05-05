8b4960ac7574a3eea3747a608524a56b1a320d26
Suhan Gui <suhangui@hanovernorwichschools.org>
2017-05-05 13:32:36 -0400

    Updated from Brython Server: 5/5/2017 1:32:34 PM

--- project.py
+++ project.py
@@ -3,15 +3,15 @@ from random import randint, shuffle
 trees = ['M','M','M','O','O','O','O','B','B','B','B','B']
 
 failures=0
-trials=10
+trials=100
 
 for i in range(trials):
     shuffle(trees)
-    print(trees)
-    for j in range(trials-1):
+    #print(trees)
+    for j in range(9):
         if trees[j] == 'B' and trees[j+1] == 'B' and trees[j+2] == 'B' and trees[j+3] == 'B':
-            print("fail")
+            #print("fail")
             failures += 1
             break
 
-print(failures)
+print((trials-failures)/trials, '%')
