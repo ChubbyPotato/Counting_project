1b2ff2c6cded2748ead567a1c1a0c83d0ae118c7
Suhan Gui <suhangui@hanovernorwichschools.org>
2017-05-05 13:25:13 -0400

    Updated from Brython Server: 5/5/2017 1:25:07 PM

--- project.py
+++ project.py
@@ -1,6 +1,12 @@
 from random import randint, shuffle
 
-print(randint(0,1))
-
 trees = ['M','M','M','O','O','O','O','B','B','B','B','B']
 
+trials=10
+
+for i in range(trials):
+    shuffle(trees)
+    print(trees)
+    for j in range(trials-1):
+        if trees[j] == 'B' and trees[j+1] == 'B' and trees[j+2] == 'B' and trees[j+3] == 'B':
+            print("fail")
