01c6a23d5a93eabfcb5a2a41a8fc486c6df57f1e
Suhan Gui <suhangui@hanovernorwichschools.org>
2017-05-06 14:17:49 -0400

    Updated from Brython Server: 5/6/2017 2:17:34 PM

--- project.py
+++ project.py
@@ -1,21 +1,37 @@
 from random import randint, shuffle
 
+
 cards=['q','q','q','q','k','k','k','k','j','j','j','j']
-trials=10000
+cardlen= len(cards)-1
+trials=10
 ya=0
 
 for i in range(trials):
     shuffle(cards)
-    for x in range(0,11):
+    for x in range(cardlen):
         if cards[x] =='q' and cards[x+1] == 'q' and cards[x+2] == 'q' and cards[x+3] == 'q':
             ya+=1
-            break
         elif cards[x] =='k' and cards[x+1] == 'k' and cards[x+2] == 'k' and cards[x+3] == 'k':
             ya+=1
-            break
         elif cards[x] =='j' and cards[x+1] == 'j' and cards[x+2] == 'j' and cards[x+3] == 'j':
             ya+=1
-            break
         else:
             break
 print(ya)
+print(100*ya/trials, '%')
+
+trials2=10
+
+c=['c']
+b=['t']
+lis=8*c+3*b
+lislen=len(lis)-1
+ya=0
+
+for i in range(trials2):
+    shuffle(lis);
+    for j in range(lislen):
+        if lis[x] == 'c' and lis[x+1] == 'c':
+            print(True)
+        else:
+            print(False)
